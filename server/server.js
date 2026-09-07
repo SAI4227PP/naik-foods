8b9dc76fbb61d7c9c3cc5390dda19515553129ab
SAI4227PP <hip6379@gmail.com>
2026-09-07 21:21:45 +0530

    deployed

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173' || 'https://naikclient.vercel.app/',
   }),
 )
 

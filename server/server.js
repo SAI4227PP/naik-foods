25bcbe42988104e92af65f0ae9f0e53be750d045
kethini Mohan sai <hip6379@gmail.com>
2026-09-07 21:42:00 +0530

    fix: allow deployed frontend CORS origin

--- server/server.js
+++ server/server.js
@@ -16,9 +16,23 @@ dotenv.config()
 
 const app = express()
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://naikclient.vercel.app',
+  process.env.CLIENT_URL,
+].filter(Boolean)
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true)
+      }
+
+      return callback(new Error('Not allowed by CORS'))
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 )
 
